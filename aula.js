2766aeb25aa9fc01652b817fa9878d74de394c69
mingau650 <dioniziorosimeire98@gmail.com>
2025-10-25 16:58:00 +0000

    a

--- aula.js
+++
@@ -1,9 +0,0 @@
-const nome = "Rafael" //PRECISA ESTAR COM ASPAS SE TIVER LETRA
-var idade = 10
-var ano = 2000
-
-while(ano <=2010) {
-    idade++
-};
-
-console.log(idade) //me fala oq vc acha q esse código faz
